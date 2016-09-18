b86b7a0530d121dca33f22cfa393fa2dad34eb46
David Smiley <DGSmiley77@gmail.com>
2016-09-18 00:26:55 -0400

    continue

--- src/lab7/invalidInputTest.java
+++ src/lab7/invalidInputTest.java
@@ -1,5 +1,6 @@
 package lab7;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class invalidInputTest {
@@ -15,22 +16,30 @@ public class invalidInputTest {
 
 		// Common town
 		String hometown = "Detroit, MI";
-		int choice;
-		System.out.println("Please enter a number between 1-20: ");
-		choice = input.nextInt();
+		int choice = 0;
+		String option;
 
-		if (choice <= 5 ) {
-			System.out.println(people[0]); 
-		} else if (choice <= 10) {
-			System.out.println(people[1]);
-		} else if (choice <= 15) {
-			System.out.println(people[2]);
-		} else if (choice <= 20) {
-			System.out.println(people[3]);
-		} else  {
-			System.out.println("Try again, enter a number between 1-20 this time."); 
-		} 
-		
-	
+		do {
+			System.out.println("Please enter a number between 1-20: ");
+			choice = input.nextInt();
+			{
+				//System.out.println("Invalid entry: Try Again");
+			}
+			if (choice <= 5) {
+				System.out.println("What do you want to know about " + people[0]);
+			} else if (choice <= 10) {
+				System.out.println("What do you want to know about " + people[1]);
+			} else if (choice <= 15) {
+				System.out.println("What do you want to know about " + people[2]);
+			} else if (choice <= 20) {
+				System.out.println("What do you want to know about " + people[3]);
+			} else {
+				System.out.println("How about we try this again");	
+				continue;
+				//choice = input.nextInt();	
+				 }			
+		} while (choice > 20);		
+		System.out.println("Home Town or Favorite Food? ");
+		option = input.nextLine();
 	}
 }
